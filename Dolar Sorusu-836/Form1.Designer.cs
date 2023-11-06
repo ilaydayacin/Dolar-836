d93434e601e89a931d7777ef5e7730fa486673f0
ilayda yalçın <146716704+ilaydayacin@users.noreply.github.com>
2023-11-06 11:27:58 +0300

    Add files via upload

---
+++ Dolar Sorusu-836/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Dolar_Sorusu_836
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.txtAdet = new System.Windows.Forms.TextBox();
+            this.txtBirim = new System.Windows.Forms.TextBox();
+            this.btnCevir = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(37, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Adet";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(37, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Birim";
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(37, 177);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(21, 20);
+            this.lblSonuc.TabIndex = 2;
+            this.lblSonuc.Text = "Tl";
+            // 
+            // txtAdet
+            // 
+            this.txtAdet.Location = new System.Drawing.Point(107, 28);
+            this.txtAdet.Name = "txtAdet";
+            this.txtAdet.Size = new System.Drawing.Size(184, 20);
+            this.txtAdet.TabIndex = 3;
+            // 
+            // txtBirim
+            // 
+            this.txtBirim.Location = new System.Drawing.Point(107, 69);
+            this.txtBirim.Name = "txtBirim";
+            this.txtBirim.Size = new System.Drawing.Size(184, 20);
+            this.txtBirim.TabIndex = 4;
+            // 
+            // btnCevir
+            // 
+            this.btnCevir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnCevir.Location = new System.Drawing.Point(267, 105);
+            this.btnCevir.Name = "btnCevir";
+            this.btnCevir.Size = new System.Drawing.Size(153, 92);
+            this.btnCevir.TabIndex = 5;
+            this.btnCevir.Text = "Çevir";
+            this.btnCevir.UseVisualStyleBackColor = true;
+            this.btnCevir.Click += new System.EventHandler(this.btnCevir_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(581, 337);
+            this.Controls.Add(this.btnCevir);
+            this.Controls.Add(this.txtBirim);
+            this.Controls.Add(this.txtAdet);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.TextBox txtAdet;
+        private System.Windows.Forms.TextBox txtBirim;
+        private System.Windows.Forms.Button btnCevir;
+    }
+}
+
---
+++ Dolar Sorusu-836/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dolar_Sorusu_836
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCevir_Click(object sender, EventArgs e)
+        {
+            double adet, birim, sonuc; 
+            adet = Convert.ToDouble(txtAdet.Text);
+            birim = Convert.ToDouble(txtBirim.Text);
+
+            sonuc = birim * adet;
+
+            lblSonuc.Text = sonuc.ToString(); 
+        }
+    }
+}
